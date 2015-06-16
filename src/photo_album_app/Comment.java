3d6022375145fd27e9591bb5678b86991b0d30b6
Evangelia Koleli <evau2music@hotmail.com>
2015-06-16 13:00:25 +0100

    initializing project

---
+++ src/photo_album_app/Comment.java
@@ -0,0 +1,9 @@
+package photo_album_app;
+
+import org.joda.time.DateTime;
+
+public class Comment {
+	private int photo_id;
+	private String text;
+	private DateTime created_at;
+}
---
+++ src/photo_album_app/Photo.java
@@ -0,0 +1,14 @@
+package photo_album_app;
+
+import java.util.ArrayList;
+
+import org.joda.time.DateTime;
+
+public class Photo {
+	
+	private String title;
+	private String photo_path;
+	private DateTime created_at;
+	private ArrayList<Comment> comments;
+
+}
